9a067674faacf626ace965a4bc7183e26fd0b24a
code <owengot@gmail.com>
2019-09-28 20:41:07 +0200

    deploy

--- app.js
+++ app.js
@@ -170,6 +170,13 @@ app.get('/', function (req, res) {
  res.send(JSON.stringify({ Hello: 'World'}));
 });
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
 app.listen(port, function () {
  console.log('Example app listening on port !');
